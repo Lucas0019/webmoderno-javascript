51061bbd90bacde571b9709909b5b13c80b2f546
Lucas <lxavier.00@outlook.com>
2020-07-07 17:54:27 -0300

    Codigo add

---
+++ 01 - Fundamentos Js/19_usandoLet1.js
@@ -0,0 +1,16 @@
+var numero = 1
+
+{
+    let numero = 2
+    console.log('Numero dentro: ', numero)
+        //2
+}
+
+console.log('Numero Fora: ', numero)
+    // 1
+
+//Resultado:
+//Numero dentro:  2
+//Numero Fora:  1
+
+//Como o Let é uma variavel que trabalha com escopo , foi possivel respeitar as sentenças de codigo
