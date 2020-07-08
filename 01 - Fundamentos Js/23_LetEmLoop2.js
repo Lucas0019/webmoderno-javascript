ca52e93552f47f5a8292e68abbc75f5f8e5c1a11
Lucas <lxavier.00@outlook.com>
2020-07-08 10:13:27 -0300

    codigo Add

---
+++ 01 - Fundamentos Js/23_LetEmLoop2.js
@@ -0,0 +1,15 @@
+const funcs = []
+
+for (let i = 0; i < 10; i++) {
+    //Aqui dentro do laço iremos preencher o array
+    funcs.push(function() {
+        console.log(i);
+
+    })
+
+}
+
+funcs[2]() //2
+funcs[8]() //8
+
+// o valor retornado para ambas é 2 e 8, pois é como se tivesse uma memoria no let , pois está contido num bloco de codigo
