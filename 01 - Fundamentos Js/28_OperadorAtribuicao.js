4c7a3f0de1c65419a501b9472c318edf1e1fbfba
Lucas <lxavier.00@outlook.com>
2020-07-09 12:01:37 -0300

    2a Add

---
+++ 01 - Fundamentos Js/28_OperadorAtribuicao.js
@@ -0,0 +1,29 @@
+const a = 7
+let b = 3
+
+//Atribuição de adição
+// b = b + a 
+//10
+b += a
+console.log(b);
+
+//Atribuição de subtração
+// b = b - a 
+b -= 4
+console.log(b);
+
+//Atribuição de multiplicação
+// b = b * a 
+b *= 2
+console.log(b);
+
+
+//Atribuição de divisão
+// b = b / a 
+b /= 2
+console.log(b);
+
+//Atribuição de resto
+// b = b % a
+b %= 2
+console.log(b);
