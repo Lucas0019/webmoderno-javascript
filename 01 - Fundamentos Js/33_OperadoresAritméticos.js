6b94257f84e1b55915434df3444f776cb65f5dc9
Lucas <lxavier.00@outlook.com>
2020-07-09 22:32:01 -0300

    3a Add

--- 01 - Fundamentos Js/33_OperadoresAritméticos.js
+++ 01 - Fundamentos Js/33_OperadoresAritméticos.js
@@ -5,9 +5,10 @@ const [a, b, c, d] = [3, 5, 1, 25]
 
 const soma = a + b + c + d //34
 const sub = d - b //20
-const mult = a * b //8.33
-const div = d / a //1 IMPAR
-const rest = a % 2 // Muitas vezes utilizado para saber se o numero é PAR ou IMPAR , se o resto for 1 impar e 0 par
+const mult = a * b //15
+const div = d / a //8.33
+const rest = a % 2 // 1 Impar
+    // O resto muitas vezes utilizado para saber se o numero é PAR ou IMPAR , se o resto for 1 impar e 0 par
 
 console.log(soma, sub, mult, div, rest)
 
