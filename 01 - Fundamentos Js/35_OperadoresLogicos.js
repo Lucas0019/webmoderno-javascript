8b288b139e01b39f955e12a555c3fe3b33bbff18
Lucas <lxavier.00@outlook.com>
2020-07-13 18:34:45 -0300

    2a Alteração

--- 01 - Fundamentos Js/35_OperadoresLogicos.js
+++ 01 - Fundamentos Js/35_OperadoresLogicos.js
@@ -1,23 +1,39 @@
-//Nota de Aula
+/**Nota de Aula
+ *
+ * V e V = V
+ * V e F = F
+ * F e ? = F
+ *Então precisamos que pelo menos um operando seja verdadeiro para o resultado final ser verdadeiro 
+ * V ou ? = V
+ * F ou V = V
+ * F ou F = F
+ *
+ *Ou Exclusivo
+ * V xor V = F
+ * V xor F = V
+ * F xor V = V
+ * F xor F = F
+ *
+ * Negação Logica em Js
+ * !v = F
+ * !f = V
+ */
 
-// V e V = V
-// V e F = F
-// F e ? = F
+//EXEMPLO QUE IREMOS TRABALHAR
 
-//Então precisamos que pelo menos um operando seja verdadeiro para o resultado final ser verdadeiro 
-
-// V ou ? = V
-// F ou V = V
-// F ou F = F
-
-//Ou Exclusivo
-
-// V xor V = F
-// V xor F = V
-// F xor V = V
-// F xor F = F
-
-// Negação Logica em Js
-
-// !v = F
-// !f = V
+/**Situação: Você tem dois trabalhos numa determinada semana, um na terça e o outro na quinta
+ * Se os dois derem certo e você receber o seu salario, terá a possibilidade de comprar uma TV de 50 polegadas e comprar um sorvete,
+ *  ESSE È O OPERADOR IN
+ *
+ * Caso contrário ! se pelo menos um dos trabalhos ( o da terça ou quinta) não derem certo , irá comparar somente uma tv de 32 polegadas
+ *
+ * e se NÂO der certos os dois trabalhos não terá como comparar nenhuma TV
+ * 
+ * o OU será o sorvete
+ *
+ * Lembrando:
+ *  --> IN (TV 50 POLEGADAS)
+ *  --> OU EXCLUSIVO (TV 32 POLEGADAS)
+ *  --> OU (Sorvete)
+ *
+ */
