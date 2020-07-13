b39734ab9c8f0aabb09d871d4c2ebee95e6b36f4
Lucas <lxavier.00@outlook.com>
2020-07-13 16:19:23 -0300

    1a Alteração

---
+++ 01 - Fundamentos Js/35_OperadoresLogicos.js
@@ -0,0 +1,23 @@
+//Nota de Aula
+
+// V e V = V
+// V e F = F
+// F e ? = F
+
+//Então precisamos que pelo menos um operando seja verdadeiro para o resultado final ser verdadeiro 
+
+// V ou ? = V
+// F ou V = V
+// F ou F = F
+
+//Ou Exclusivo
+
+// V xor V = F
+// V xor F = V
+// F xor V = V
+// F xor F = F
+
+// Negação Logica em Js
+
+// !v = F
+// !f = V
