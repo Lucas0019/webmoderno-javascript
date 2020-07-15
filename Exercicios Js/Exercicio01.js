b4510ea764e729bc35a8410ce6924c922a4323f7
Lucas <lxavier.00@outlook.com>
2020-07-15 19:55:08 -0300

    Alteração na lista

--- Exercicios Js/Exercicio01.js
+++ Exercicios Js/Exercicio01.js
@@ -1,1 +1,6 @@
 /* 1) Faça um laço for de 0 a 100 que retorne um array com todos os números que são divisíveis por 7; */
+
+for (var i = 0; i < 9; i++) {
+    console.log(i);
+    // more statements
+}
