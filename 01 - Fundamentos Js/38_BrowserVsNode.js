ad90624d89bdb81af3b967b9f2a8cd19d84387e3
Lucas <lxavier.00@outlook.com>
2020-07-16 13:14:24 -0300

    1a Alteraçao

---
+++ 01 - Fundamentos Js/38_BrowserVsNode.js
@@ -0,0 +1,6 @@
+/**
+ * Analisando o contexto de Execução
+ *
+ *Front End : o javaScript é executado no browser
+ * Back End: Que é quando está executando fora do navegador, utiliza-se um runtime ''Node.js 
+ */
