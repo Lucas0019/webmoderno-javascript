332d57cff6593b2d09835e39ce92040642c05e2f
Lucas <lxavier.00@outlook.com>
2020-07-15 22:50:02 -0300

    2a Alteraçao

--- 01 - Fundamentos Js/36_OperadoresUnarios.js
+++ 01 - Fundamentos Js/36_OperadoresUnarios.js
@@ -1,3 +1,14 @@
 //Um operador unário é uma operação com apenas um operando.
 
 /**Veremos agora o Incremento e o Decremento  */
+let num1 = 1
+let num2 = 2
+
+//Nota de Aula : Atentar-se a precedência dos sinais 
+// Forma pos fixada, esse operando acrescenta um valor a unidade
+num1++
+console.log(num1); //2
+
+//Forma prefixada, esse operando decrementa o valor da unidade
+--num1
+console.log(num1); //1
