2435a4fbd28421850b99a3b2681eee19a8e43d15
Lucas <lxavier.00@outlook.com>
2020-07-22 10:30:04 -0300

    2a Alteraçao

--- 02 - Estrutura de Contole Js/43_EstruturaIF-ELSEIF.js
+++ 02 - Estrutura de Contole Js/43_EstruturaIF-ELSEIF.js
@@ -1,1 +1,32 @@
 //Função de comparação entre duas notas com possíveis retornos
+
+Number.prototype.entre = function(inicio, fim) {
+    return this >= inicio && this >= fim //O this usado para acessar o Number
+}
+
+const imprimirResultado = function(nota) {
+    if (nota.entre(9, 10)) {
+        console.log('Quadro de Honrá');
+    } else if (nota.entre(7, 8.99)) {
+        console.log('Aprovado');
+
+    } else if (nota.entre(4, 6.99)) {
+        console.log('Recuperação');
+
+    } else if (nota.entre(0, 3.99)) {
+        console.log('Recuperação');
+
+    } else { //Quando a nota é invalida
+        console.log('Nota Invalida');
+    }
+    //console.log('Fim');
+
+}
+
+//Aplicando os testes
+imprimirResultado(10)
+imprimirResultado(8)
+imprimirResultado(6)
+imprimirResultado(3)
+imprimirResultado(-1)
+imprimirResultado(11)
