6a2c9e328d289460f325c48e0971d338475fc6c5
Lucas <lxavier.00@outlook.com>
2020-07-22 19:42:23 -0300

    1a Alteraçao

---
+++ 02 - Estrutura de Contole Js/47_EstruturaFOR.js
@@ -0,0 +1,9 @@
+// A instrução for cria um loop que consiste em três expressões opcionais,
+//  dentro de parênteses e separadas por ponto e vírgula, 
+//  seguidas por uma declaração ou uma sequência de declarações executadas em sequência.
+
+//Sintaxe: for ([inicialização]; [condição]; [expressão final])
+//declaração
+
+
+//Iremos ver dois caso um com While e outro com FOR para entendermos as diferenças
