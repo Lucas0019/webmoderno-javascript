12b5f6fd923423ab51c962578aa119af6a997239
Lucas <lxavier.00@outlook.com>
2020-07-22 16:28:45 -0300

    1a Alteraçao

--- 02 - Estrutura de Contole Js/45_EstruturaWhile.js
+++ 02 - Estrutura de Contole Js/45_EstruturaWhile.js
@@ -1,2 +1,19 @@
 //A declaração while cria um laço que executa uma rotina especifica enquanto a condição de teste for avaliada como verdadeira. 
 //A condição é avaliada antes da execução da rotina.
+
+function getNumeroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+
+}
+
+let numero = 0
+
+//A ideia é que enquanto a operação verdadeira , irá executar o laço de repetição
+while (numero != -1) {
+    numero = getNumeroAleatorioEntre(-1, 10)
+    console.log(`O numero  escolhida foi ${numero}.`);
+
+}
+
+console.log('Fim da Execução');
