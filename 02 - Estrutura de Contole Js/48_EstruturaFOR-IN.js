3a7a8a3dbaad1c5ba97414381da08c7d1695c5b2
Lucas <lxavier.00@outlook.com>
2020-07-23 16:26:10 -0300

    1a Alteraçao

---
+++ 02 - Estrutura de Contole Js/48_EstruturaFOR-IN.js
@@ -0,0 +1,4 @@
+// O laço for...in  interage sobre propriedades enumeradas de um objeto, na ordem original de inserção.  
+// O laço pode ser executado para cada propriedade distinta do objeto
+
+//A ordem não importa
