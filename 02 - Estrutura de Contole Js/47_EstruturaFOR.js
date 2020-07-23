a830caf21c899240c20e303bb921a489887b65eb
Lucas <lxavier.00@outlook.com>
2020-07-23 16:08:31 -0300

    2a Alteraçao

--- 02 - Estrutura de Contole Js/47_EstruturaFOR.js
+++ 02 - Estrutura de Contole Js/47_EstruturaFOR.js
@@ -7,3 +7,25 @@
 
 
 //Iremos ver dois caso um com While e outro com FOR para entendermos as diferenças
+
+
+//Usando o while 1 ao 10
+//Quantidade determinada
+let contador = 1
+    //Quando o contador for menor que 10, ele sai do laço while
+while (contador <= 10) {
+    console.log(`contador =  ${contador}`);
+    contador++ //incremento
+}
+
+//Usando o FOR 1 ao 10
+for (let i = 1; i <= 10; i++) {
+    console.log(`i = ${i}`);
+}
+
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+for (let i = 0; i < notas.length; i++) {
+    console.log(`Nota = ${notas[i]}`);
+
+}
