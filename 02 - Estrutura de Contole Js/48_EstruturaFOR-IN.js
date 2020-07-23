1ebe72b6b6308850ed52243c721003703f7b64a9
Lucas <lxavier.00@outlook.com>
2020-07-23 16:45:15 -0300

    1a Alteraçao

--- 02 - Estrutura de Contole Js/48_EstruturaFOR-IN.js
+++ 02 - Estrutura de Contole Js/48_EstruturaFOR-IN.js
@@ -2,3 +2,29 @@
 // O laço pode ser executado para cada propriedade distinta do objeto
 
 //A ordem não importa
+//O FOR...IN retorna o ÍNDICE de cada elemento (Posição dele)
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+
+for (let i in notas) {
+    console.log(i, notas[i]);
+}
+//Retorna
+
+// 0 6.7
+// 1 7.4
+// 2 9.8
+// 3 8.1
+// 4 7.7
+
+const pessoa = {
+    nome: 'Lucas',
+    sobrenome: 'Xavier',
+    idade: 20,
+    peso: 71
+
+}
+
+for (let atributo in pessoa) {
+
+    console.log(`${atributo} = ${pessoa[atributo]}`);
+}
