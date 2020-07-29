5fa1191545b1142b044132ca70ea9b9f61552a55
Lucas <lxavier.00@outlook.com>
2020-07-29 15:15:39 -0300

    1a Alteraçao

---
+++ 03 - Mais_Funcao JS/52_ParametrosVariaveis.js
@@ -0,0 +1,18 @@
+/**Forma antes no novo ECMAScript */
+
+function soma() {
+    let soma = 0
+
+    // O objeto arguments é como um objeto Array correspondendo aos argumentos passados para uma função.
+    for (i in arguments) {
+        soma += arguments[i]
+    }
+    return soma
+
+}
+
+console.log(soma()); //0
+console.log(soma(1)); //1
+console.log(soma(1.1, 2.2, 3.3)); //6.6
+console.log(soma(1.1, 2.2, 'Texto')); // 3.3000000000000003Texto
+console.log(soma('A', 'B', 'C')); //0ABC
