f00dcbfdc7e6ca97c4d226234727d6f43bc1a02f
Lucas <lxavier.00@outlook.com>
2020-07-31 21:45:51 -0300

    1a Alteraçao

---
+++ 03 - Mais_Funcao JS/57_FuncaoArrow-01.js
@@ -0,0 +1,32 @@
+/*Arrow Function
+ * 
+ * A principal diferença é o contexto do this. A arrow function não cria o próprio this
+ * Uma expressão arrow function possui uma sintaxe mais curta quando comparada a uma expressão de função (function expression) 
+ * e não tem seu próprio this, arguments, super ou new.target. Estas expressões de funções são melhor aplicadas para funções que não sejam métodos,
+ *  e elas não podem ser usadas como construtoras (constructors).
+ *Introduzida no ES6 2015
+ */
+
+//Função Normal 
+let dobro = function(a) {
+    return a * 2
+}
+
+//Arrow Function
+dobro = (a) => {
+    return a * 2
+}
+
+//Arrow Function mais reduzida
+dobro = a => 2 * a //return implícita
+
+console.log(dobro(6));
+
+//Outro exemplo
+let ola = function() {
+    return 'Olá Mundo'
+}
+
+ola = () => 'Ola'
+ola = _ => 'Ola' // Possui um parâmetro
+console.log(ola());
