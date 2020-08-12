321d840b9fd50bbd8f8f037a00d8f47d47b65908
Lucas <lxavier.00@outlook.com>
2020-08-12 15:19:23 -0300

    2a Alteraçao

--- 03 - Mais_Funcao JS/61_FuncaoCallback.js
+++ 03 - Mais_Funcao JS/61_FuncaoCallback.js
@@ -1,2 +1,26 @@
 // Uma função callback é uma função passada a outra função como argumento, 
 // que é então invocado dentro da função externa para completar algum tipo de rotina ou ação.
+
+const fabricantes = ['Mercedes', 'Audi', 'BMW']
+
+function imprimir(nome, indice) {
+    console.log(`${indice +1}. ${nome}`);
+}
+
+fabricantes.forEach(imprimir)
+    // Resultado
+
+// 1. Mercedes
+// 2. Audi
+// 3. BMW
+
+console.log('\n')
+
+fabricantes.forEach(function(fabricante) {
+    console.log(fabricante);
+})
+
+console.log('\n');
+
+//EM uma função arrow
+fabricantes.forEach(fabricante => console.log(fabricante))
