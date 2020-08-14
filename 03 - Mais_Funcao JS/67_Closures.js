5c36747edba67aefb64ebdfb4565941092d0a75e
Lucas <lxavier.00@outlook.com>
2020-08-14 18:16:22 -0300

    1a Alteraçao

---
+++ 03 - Mais_Funcao JS/67_Closures.js
@@ -0,0 +1,19 @@
+//Closure é o escopo criado quando uma função é declarada
+//Esse escopo permite a função acessar e manipular variáveis externas à função
+//Memoria do local de origem
+
+//contexto lexico em ação
+
+const x = 'Global';
+
+function fora() {
+    const x = 'Local';
+
+    function dentro() {
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora();
+console.log(minhaFuncao());
