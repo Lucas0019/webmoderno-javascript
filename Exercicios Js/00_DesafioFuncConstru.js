83b063ff4e310a110a169983b23e57d9b25b5012
Lucas <lxavier.00@outlook.com>
2020-08-15 18:19:04 -0300

    Refatoraçao

---
+++ Exercicios Js/00_DesafioFuncConstru.js
@@ -0,0 +1,10 @@
+//Portar para uma função construtor 
+class Pessoa {
+    constructor(nome) {
+            this.nome = nome
+        }
+        //Método
+    falar() {
+        console.log(`Meu nome é ${this.nome}`);
+    }
+}
