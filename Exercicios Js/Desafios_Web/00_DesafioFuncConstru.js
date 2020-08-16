4ee168ed69b6e89afe95244dd417df8b848cd0b0
Lucas <lxavier.00@outlook.com>
2020-08-15 23:26:30 -0300

    Desafio Concluido

--- Exercicios Js/Desafios_Web/00_DesafioFuncConstru.js
+++ Exercicios Js/Desafios_Web/00_DesafioFuncConstru.js
@@ -1,10 +1,24 @@
 //Portar para uma função construtor 
-class Pessoa {
-    constructor(nome) {
-            this.nome = nome
-        }
-        //Método
-    falar() {
-        console.log(`Meu nome é ${this.nome}`);
+// class Pessoa {
+//     constructor(nome) {
+//             this.nome = nome
+//         }
+//         //Método
+//     falar() {
+//         console.log(`Meu nome é ${this.nome}`);
+//     }
+// }
+
+//Fica Assim
+function Pessoa(nome) {
+    this.nome = nome
+
+    this.falar = function() {
+        console.log(`Meu nome é ${this.nome}`)
     }
 }
+
+//Executando 
+const p1 = new Pessoa('Lucas Xavier')
+p1.falar()
+console.log(p1.nome)
