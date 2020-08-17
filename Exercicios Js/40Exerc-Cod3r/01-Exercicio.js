dd14994881c2c0958b017e545b22c262b0b89b39
Lucas <lxavier.00@outlook.com>
2020-08-17 14:30:23 -0300

    Exerc 01 ok

---
+++ Exercicios Js/40Exerc-Cod3r/01-Exercicio.js
@@ -0,0 +1,10 @@
+/**
+ * 01)​ Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+ * multiplicação e divisão desses valores.
+ */
+
+function calculoOpera(op1, op2) {
+    console.log(op1 + op2, op1 - op2, op1 * op2, op1 / op2);
+}
+
+calculoOpera(2, 2) //4 0 4 1
