c84a084778b745837712802ba9e2d61054124355
Lucas <lxavier.00@outlook.com>
2020-08-17 18:23:19 -0300

    Exerc 04 ok

--- Exercicios Js/40Exerc-Cod3r/04-Exercicio.js
+++ Exercicios Js/40Exerc-Cod3r/04-Exercicio.js
@@ -3,3 +3,10 @@
  *  o dividendo e o divisor. A função deverá imprimir o resultado
  * e o resto da divisão destes dois valores
  */
+
+function divisao(dividendo, divisor) {
+    console.log("Resultado: " + Math.floor(dividendo / divisor));
+    console.log(`Resto: ${dividendo % divisor}`)
+}
+
+divisao(14, 3)
