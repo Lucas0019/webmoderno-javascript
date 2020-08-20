b31936c4a11e34df2b023575bd9e30adbbc4ce1a
Lucas <lxavier.00@outlook.com>
2020-08-20 18:20:19 -0300

    Exerc 06 add

---
+++ Exercicios Js/40Exerc-Cod3r/06-Exercicio.js
@@ -0,0 +1,6 @@
+/**
+ * 06)​ Elabore duas funções que recebem três parâmetros: 
+ * capital inicial, taxa de juros e tempo de aplicação. 
+ * A primeira função retornará o montante da aplicação financeira sob o regime de juros simples
+ *  e a segunda retornará o valor da aplicação sob o regime de juros compostos.
+ */
