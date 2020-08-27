4138307bf2d614fffc47ab2c67be7a6f2d6437b0
Lucas <lxavier.00@outlook.com>
2020-08-27 19:04:12 -0300

    Add aula 73

--- 04-Orientacao-a-Objeto/73_Introducao-OObjetos.js
+++ 04-Orientacao-a-Objeto/73_Introducao-OObjetos.js
@@ -1,1 +1,22 @@
-//CONTEXTO TEÓRICO
+//CONTEXTO TEÓRICO - CÓDIGO NÂO EXECUTÁVEL
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Princípios importantes:
+// 1. abstração
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo
