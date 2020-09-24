7c5a0ac3a3f100ede8f0064a84500f66ae5f6b11
Lucas <lxavier.00@outlook.com>
2020-09-24 17:54:36 -0300

    add aulas novas

--- 04-Orientacao-a-Objeto/77-Notacao-Literal.js
+++ 04-Orientacao-a-Objeto/77-Notacao-Literal.js
@@ -9,17 +9,43 @@ const c = 3;
 const obj1 = {
     a: a,
     b: b,
-    c: c
+    c: c,
 };
 
 //Depois
 const obj2 = {
     a,
     b,
-    c
-}
+    c,
+};
 
 console.log(obj1, obj2); //{ a: 1, b: 2, c: 3 } { a: 1, b: 2, c: 3 }
 
+//Criar dinamicamente um atributo em um objeto usando uma String
 
-//Criar dinamicamente um atributo em um objeto
+const nomeAttr = "nota";
+const valorAttr = 7.87;
+
+const obj3 = {};
+obj3[nomeAttr] = valorAttr;
+console.log(obj3); //{ nota: 7.87 }
+
+//nova forma
+const obj4 = {
+    [nomeAttr]: valorAttr,
+};
+console.log(obj4); //{ nota: 7.87 }
+
+//Funções em Objeto
+const obj5 = {
+    function1: function() {
+        //...
+        console.log("Fala Dev");
+    },
+    //Forma nova ECMAScript 2015 - Reduzida
+    function2() {
+        //...
+        console.log("Opa Dev");
+    },
+};
+console.log(obj5); //{ function1: [Function: function1], function2: [Function: function2] }
---
+++ 04-Orientacao-a-Objeto/78-Getters-Setters.js
@@ -0,0 +1,9 @@
+//Getters: A sintaxe de get associa uma propriedade de um objeto a uma função que será chamada quando tal propriedade é acessada.
+// prop - O nome da propriedade à qual a função dada será associada.
+// expression - A partir do ECMAScript 6, você pode também utilizar expressões para um nome computado de uma propriedade para associar à função dada.
+
+
+//Setters - A sintaxe set liga a propriedade da função para ser chamada quando existe uma tentativa de definir valor para essa propriedade.
+// prop - Nome da propriedade ligada a função.
+// val - Um apelido para variável que será atribuidada a prop.
+// expression -Iniciando com ECMAScript 6, você pode usar também expressões para computar o nome da propriedade a ser definida na função.
